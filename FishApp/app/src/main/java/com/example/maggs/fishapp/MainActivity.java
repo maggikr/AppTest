5c245800e5a182fa1a13a8ec70796fb7a15aa9ec
maggikr <magnusk88@gmail.com>
2017-10-17 20:18:38 +0200

    Added:  Map UI components  Changed from supportMapFragment to MapFragment  EasyPermissions

--- FishApp/app/src/main/java/com/example/maggs/fishapp/MainActivity.java
+++ FishApp/app/src/main/java/com/example/maggs/fishapp/MainActivity.java
@@ -1,51 +1,53 @@
 package com.example.maggs.fishapp;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap gMap;
 
+    //Variabler for å sjekke permission
+    private String[] locationPermission = {Manifest.permission.ACCESS_FINE_LOCATION};
+    private static final int LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_map);
-        // Get the SupportMapFragment and request notification
-        // when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //Legger til egen toolbar øverst i app
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
 
-
-
     }
 
+    // Laster inn meny/søkeknapp i toolbar
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
-
-        MenuItem searchItem = menu.findItem(R.id.item_search);
-        SearchView searchView =
-                (SearchView) MenuItemCompat.getActionView(searchItem);
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -54,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.item_login:
                 startActivity(new Intent(this, LoginActivity.class));
                 return true;
@@ -79,22 +81,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Manipulates the map when it's available.
-     * The API invokes this callback when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user receives a prompt to install
-     * Play services inside the SupportMapFragment. The API invokes this method after the user has
-     * installed Google Play services and returned to the app.
-     */
+    // Kjøres når kartet er lastet inn
+    // Legger til "marker" i halden og zoomer inn på lokasjonen
+    // Kjører også metoder for å sjekke at det er gitt permission til brukers lokasjon
+    // og aktiverer UI komponenter i kartet som "My location" knapp og +/- zoom
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
+        gMap = googleMap;
+
         LatLng halden = new LatLng(59.12478, 11.38754);
-        googleMap.addMarker(new MarkerOptions().position(halden)
+        gMap.addMarker(new MarkerOptions().position(halden)
                 .title("Marker in Halden"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(halden));
+        gMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(halden, 15, 0, 0)));
+
+        setLocationEnabled();
+        setDefaultUiSettings();
+    }
+
+    private void setDefaultUiSettings() {
+        UiSettings uiSettings = gMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setMapToolbarEnabled(false);
+        uiSettings.setMyLocationButtonEnabled(true);
+
+    }
+
+    //Sjekker om bruker har gitt lokasjons permission, ber om permission om det ikke er gitt.
+    @AfterPermissionGranted(LOCATION_PERMISSION)
+    private void setLocationEnabled() {
+
+        if (EasyPermissions.hasPermissions(this, locationPermission)) {
+
+            gMap.setMyLocationEnabled(true);
+
+        } else {
+
+            EasyPermissions.requestPermissions(this, getString(R.string.no_location_permission),
+                    LOCATION_PERMISSION, locationPermission);
+        }
     }
 }
