4a3e36878b55e380e250ba3843dfe9597a7934b9
maggikr <magnusk88@gmail.com>
2017-11-22 18:51:30 +0100

    set up help activity

--- FishApp/app/src/main/java/com/example/maggs/fishapp/HelpActivity.java
+++ FishApp/app/src/main/java/com/example/maggs/fishapp/HelpActivity.java
@@ -1,7 +1,9 @@
 package com.example.maggs.fishapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
